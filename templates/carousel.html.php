1478cb44d1b9903ff05c66c9beac105775d35eb5
Lucas Anjos <lucas@clickjogos.com.br>
2015-03-23 15:05:58 -0300

    Uploading Site

---
+++ templates/carousel.html.php
@@ -0,0 +1,51 @@
+<div id="top-carousel" class="carousel slide" data-ride="carousel">
+  <!--bullets-->
+  <ol class="carousel-indicators">
+    <li data-target="#top-carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#top-carousel" data-slide-to="1"></li>
+    <li data-target="#top-carousel" data-slide-to="2"></li>
+  </ol>
+
+  <!--imagens-->
+
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/flor.jpg" alt="fatec">
+      <div class="carousel-caption">
+        <h1 class="h1">Vestibular</h1>
+        <p>
+          <a href="" class="btn btn-primary btn-large" role="button">Saiba Mais</a>
+        </p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/flor.jpg" alt="fatec">
+      <div class="carousel-caption">
+        <h1 class="h1">Quem Somos</h1>
+        <p>
+          <a href="" class="btn btn-primary btn-large" role="button">Ler</a>
+        </p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/flor.jpg" alt="fatec">
+      <div class="carousel-caption">
+        <h1 class="h1">Calendário</h1>
+        <p>
+          <a href="" class="btn btn-primary btn-large" role="button">Ver</a>
+        </p>
+      </div>
+    </div>
+
+    <!--Setas Laterais-->
+
+    <a class="left carousel-control" href="#top-carousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Voltar</span>
+    </a>
+    <a class="right carousel-control" href="#top-carousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Avançar</span>
+    </a>
+  </div>
+</div>
