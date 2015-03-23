3605a1ca5bae0761095128108f4a87431eb63069
LuanaInada <luana_inada@hotmail.com>
2015-03-23 18:05:04 -0300

    estagio.php
    Página do estágio

---
+++ estagio.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel='stylesheet' href='css/index.css'/>
+    <script src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="css/bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/facebook_timeline.js"></script>
+    <title>.:: Fatec Araçatuba ::.</title>
+  </head>
+  <body>
+    <?php include ("templates/menu.html.php") ?>
+    <hr>
+	<h2 class="h2 text-center">Estágio</h2>
+	<hr>
+    <div class="container">
+      
+      <h2 class="h3 text-center">Horário de Atendimento</h3>
+      <hr>
+      <div class="row">
+        <article class="content">
+         
+		  
+			
+		  
+        </article>
+      </div>
+    </div>
+	<hr>
+	
+    </div>
+  <?php include ("templates/footer.html.php") ?>
+  </body>
+</html>
