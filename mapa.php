0a3b2b90e397f425ebc6b535e3c53848c291568c
LuanaInada <luana_inada@hotmail.com>
2015-03-30 14:55:40 -0300

    Mapa da Fatec
    Mapa da Fatec

---
+++ mapa.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel='stylesheet' href='css/index.css'/>
+    <script src="js/jquery.min.js"></script>
+    <script src="css/bootstrap/js/bootstrap.min.js"></script>
+    <title>.:: Fatec Araçatuba ::.</title>
+  </head>
+  <body>
+    <?php include ("templates/menu.html.php") ?>
+	
+    <h2 class="h2 text-center">Mapa da Fatec</h2>
+	<hr>
+    <div class="container page">  
+		<?php include ("templates/mapa.html.php") ?>   
+    </div>
+	
+  <?php include ("templates/footer.html.php") ?>
+  </body>
+</html>
---
+++ templates/mapa.html.php
@@ -0,0 +1,21 @@
+<article class="content">
+
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+    <script>
+		var map;
+		function initialize() {
+			var mapOptions = {
+				zoom: 30,
+				center: new google.maps.LatLng(-21.181656, -50.450437,17)
+			};
+			map = new google.maps.Map(document.getElementById('map-canvas'),
+			mapOptions);
+		}
+
+		google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
+	
+	<div id="map-canvas"></div>
+
+</article>
