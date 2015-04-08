8a718380de97cf52a108e48e3acae2bc3acb3578
Lucas Anjos <lucas@clickjogos.com.br>
2015-04-08 16:43:30 -0300

    Página que faltou.

--- quem_somos.php
+++ quem_somos.php
@@ -1,32 +1,13 @@
 <!DOCTYPE HTML>
 <html>
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <link rel='stylesheet' href='css/index.css'/>
-    <script src="js/jquery.min.js"></script>
-    <script src="css/bootstrap/js/bootstrap.min.js"></script>
-    <script src="js/stick.js"></script>
-    <title>.:: Fatec Araçatuba ::.</title>
+    <?php include "templates/head_content.php" ?>
   </head>
   <body>
     <?php include ("templates/menu.html.php") ?>
-	
-    <h2 class="h2 text-center">Quem Somos</h2>
-   
     <div class="container main">
-      <hr>
-      <div class="row">
-        <article class="content" style="height: 290px;">
-			 <p>Vinculada ao Centro Paula Souza, a Fatec (Faculdade de Tecnologia) é uma instituição de educação superior mantida pelo governo 
-				do estado de São Paulo, assim como a Universidade de São Paulo (USP), Universidade Estadual de Campinas (Unicamp) e a Universidade 
-				Estadual Paulista (Unesp). 
-			</p>
-        </article>
-      </div>
+      
     </div>
-  <?php include ("templates/footer.html.php") ?>
+    <?php include ("templates/footer.html.php") ?>
   </body>
 </html>
