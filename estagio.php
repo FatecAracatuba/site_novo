d85dcb27c61d9e8ca20e2044a03c78c981d44d78
LuanaInada <luana_inada@hotmail.com>
2015-04-08 16:19:25 -0300

    Revert "Identação do estagio.php"
    This reverts commit f3ec84433c4f041f833d619a399d258eacb5344b.

--- estagio.php
+++ estagio.php
@@ -1,29 +1,29 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     
-		<link rel='stylesheet' href='css/index.css'/>
-		<script src="js/jquery.min.js"></script>
-		<script type="text/javascript" src="css/bootstrap/js/bootstrap.min.js"></script>
-		<script src="js/stick.js"></script>
-		<script type="text/javascript">
-			function preview(link){
-				$('div.media').remove();
-				$('.content').append('<a href="#" class="media"></a>')
-				$('a.media').attr("href", link);
-				$('a.media').media({width:770, height:1080});
-			};
-		</script>
-		<title>.:: Fatec Araçatuba ::.</title>
-	</head>
-	<body>
-		<?php include ("templates/menu.html.php") ?>
-		<div class="container page main">  
-			<?php include ("templates/estagio.html.php") ?>   
-		</div>
-		<?php include ("templates/footer.html.php") ?>
-	</body>
+    <link rel='stylesheet' href='css/index.css'/>
+    <script src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="css/bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/stick.js"></script>
+	<script type="text/javascript">
+      function preview(link){
+          $('div.media').remove();
+          $('.content').append('<a href="#" class="media"></a>')
+          $('a.media').attr("href", link);
+          $('a.media').media({width:770, height:1080});
+      };
+    </script>
+    <title>.:: Fatec Araçatuba ::.</title>
+  </head>
+  <body>
+    <?php include ("templates/menu.html.php") ?>
+    <div class="container page main">  
+      <?php include ("templates/estagio.html.php") ?>   
+    </div>
+  <?php include ("templates/footer.html.php") ?>
+  </body>
 </html>
