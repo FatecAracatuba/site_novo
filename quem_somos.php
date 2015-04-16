a195adee5d0662beb87e7faf44c8e4b936e28363
LuanaInada <luana_inada@hotmail.com>
2015-04-16 15:54:36 -0300

    Revert "Revert "Alterações e criação de páginas""
    This reverts commit 60fec79ff66a43246b572e7fb60f7d229499e387.

--- quem_somos.php
+++ quem_somos.php
@@ -6,8 +6,16 @@
   <body>
     <?php include ("templates/menu.html.php") ?>
     <div class="container main">
-      
-    </div>
+			<div class="container">
+				<h2>A Fatec de Araçatuba</h2>
+				<hr>
+				Gerida pelo <a href="http://www.centropaulasouza.sp.gov.br/">Centro Paula Souza</a>, a Fatec (Faculdade de Tecnologia) Araçatuba Professor Fernando de Amaral de Almeida
+				Prado é uma instituição pública de ensino superior, mantida pelo Governo do Estado de São Paulo. A unidade local, assim com as demais 63 espalhadas pelo 
+				estado, atua na formação de tecnólogos, profissionais de nível superior que atuam no eixo tecnológico. A instituição iniciou suas atividades em 2008, 
+				com a graduação de Bicombustíveis, em 2011, passou a oferecer o curso superior de Análise e Desenvolvimento de Sistemas. No ano de 2015, foi implantada 
+				a graduação a distância de Gestão Empresarial. 
+			</div>
+		</div>
     <?php include ("templates/footer.html.php") ?>
   </body>
 </html>
