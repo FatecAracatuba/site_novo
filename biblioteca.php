a94421e8342edde9788e864ee1f995118e5e8ff2
LuanaInada <luana_inada@hotmail.com>
2015-05-13 14:37:08 -0300

    Alteração design
    Alteração design

--- biblioteca.php
+++ biblioteca.php
@@ -6,7 +6,60 @@
   <body>
     <?php include ("templates/menu.html.php") ?>
     <div class="container main">
-
+			<div class="container">
+				<ul class="nav nav-tabs">
+          <li role="presentation" class="active">
+            <a href="#sobre" data-toggle="tab">A biblioteca</a>
+          </li>
+          <li role="presentation">
+            <a href="#horarios" data-toggle="tab">Horários de Atendimento</a>
+          </li>
+          <li role="presentation">
+            <a href="#acervo" data-toggle="tab">Acervo</a>
+          </li>
+          <li role="presentation">
+            <a href="#contato" data-toggle="tab">Contato</a>
+          </li>
+        </ul>
+				<div class="tab-content">
+          <div class="tab-pane fade in active" id="sobre">
+						<article class="content">
+							<h2>A biblioteca</h2>
+							<hr>
+							<p>A <strong>Biblioteca da FATEC</strong> atende aos alunos, professores e funcionários. Seu principal objetivo é oferecer apoio didático, 
+							científico e pedagógico, atendendo a comunidade escolar (docentes discentes e funcionários). Também é disponibilizado consulta 
+							aos materiais e acesso a internet para a comunidade externa.</p>
+							<p>A Biblioteca é um ambiente propício para leitura e pesquisa, tendo diversas fontes de informação. Seu principal objetivo 
+							é incentivar os alunos à leitura e despertar o interesse e gosto por ela.</p>
+							<p>Nosso acervo é de livre acesso, com livros, monografias, revistas, jornais, CD-ROM.</p>
+							<p>A biblioteca tem a disposição 10 computadores com acesso à internet para realização de pesquisas e trabalhos escolares. 
+							O acesso ao acervo da biblioteca é livre, onde o aluno poderá dirigir-se diretamente aos materiais ou solicitar aos funcionários.</p>
+							<p>Consulta ao acervo da biblioteca pela internet : <a href="http://www.biblioceeteps.com.br/">http://www.biblioceeteps.com.br/</a></p>
+						</article>
+					</div>
+					<div class="tab-pane fade" id="horarios">
+						<article class="content">
+							<h2>Horários de Atendimento</h2>
+							<hr>
+								
+						</article>
+					</div>
+					<div class="tab-pane fade" id="acervo">
+						<article class="content">
+							<h2>Acervo</h2>
+							<hr>
+								
+						</article>
+					</div>
+					<div class="tab-pane fade" id="contato">
+						<article class="content">
+							<h2>Contato</h2>
+							<hr>
+								
+						</article>
+					</div>
+				</div>
+			</div>
     </div>
     <?php include ("templates/footer.html.php") ?>
   </body>
