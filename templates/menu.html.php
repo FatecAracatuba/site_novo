e15e75f3d69aada91c5744d646807dd24aee2728
Lucas Anjos <oargus.g@gmail.com>
2015-06-02 17:54:21 -0300

    Monitoria

--- templates/menu.html.php
+++ templates/menu.html.php
@@ -1,10 +1,10 @@
-<!--Não mexam neste arquivo novamente, sério-->
 <div class="banner">
   <img src="images/banner_blue.png" alt="">
 </div>
-<nav class="navbar navbar-default text-center ">
+<nav class="navbar navbar-default text-center">
   <div class="container">
-    <div class="col-sm-2"></div>
+    <div class="col-sm-2">
+    </div>
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#fatec-menu">
         <span class="sr-only">Alternar Navegação</span>
@@ -23,7 +23,9 @@
           <a href="#"  class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Instituição <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
             <li><a href="quem_somos.php">Quem Somos</a></li>
+            <li><a href="turmas.php">Turmas Formadas</a></li>
             <li><a href="biblioteca.php">Biblioteca</a></li>
+            <li><a href="#">Servidores</a></li>
       			<li><a href="monitoria.php">Monitoria</a></li>
       			<li><a href="iniciacao.php">Iniciação Científica</a></li>
       			<li><a href="concursos.php">Concursos</a></li>
@@ -33,9 +35,8 @@
         <li class="dropdown">
           <a href="#"  class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Graduação <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="curso_ads.php">Análise e Desenvolvimento de Sistemas</a></li>
+            <li><a href="curso_ads.php">ADS</a></li>
             <li><a href="curso_bio.php">Biocombustíveis</a></li>
-            <li><a href="curso_gestao.php">Gestão Empresarial</a></li>
           </ul>
         </li>
         <li><a href="trabalho_graduacao.php">Trabalho de Graduação</a></li>
