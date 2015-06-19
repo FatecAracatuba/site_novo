ac525d4e6eee75426c60b3556ca819f08aefb2ea
Lucas Anjos <o.lucas.santos@live.com>
2015-06-19 10:38:38 -0300

    Update login.php

--- modules/login.php
+++ modules/login.php
@@ -2,8 +2,8 @@
   session_start();
   unset($_SESSION['logged']);
   if(isset($_POST['user']) && isset($_POST['pwd'])){
-    if($_POST['user'] == "administrador"){
-      if($_POST['pwd'] == "senha@2015@admin*123"){
+    if($_POST['user'] == "--- HÁ!"){
+      if($_POST['pwd'] == "--- HÁ!"){
         $_SESSION['logged'] = true;
         header("Location: /site_novo/restritos.php");
       }else{
