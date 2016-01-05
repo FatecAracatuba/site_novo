d8e6845eef14e1f5fed6b7c7187ce52128ec6578
LuanaInada <luana_inada@hotmail.com>
2016-01-05 18:19:21 -0200

    Mudanças no modal
    Mudanças no modal

--- templates/modal_dados_aviso.php
+++ templates/modal_dados_aviso.php
@@ -3,18 +3,14 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">Dados do Aviso</h4>
+            <h4 class="text-center modal-title">Dados do Aviso</h4>
           </div>
           <div class="modal-body">
-            <h3 id="name"></h3>
-            <hr>
-							<img class="img-responsive"></img>
-						<hr>
+            <h3 id="titulo"></h3>
             <div class="row">
-              <div class="col-xm-4 col-sm-4 col-md-4 col-lg-4"><p><strong>Titulo:</strong> <span id="titulo"></span></p></p></div>
-              <div class="col-xm-4 col-sm-4 col-md-4 col-lg-4"><p><strong>Enviado em:</strong> <span id="data_envio"></span></p></p></div>
-              <div class="col-xm-4 col-sm-4 col-md-4 col-lg-4"><p><strong>Alterado em:</strong> <span id="data_alteracao"></span></p></p></div>
-              <div class="col-xm-4 col-sm-4 col-md-4 col-lg-4"><p><strong>Conteúdo:</strong> <span id="conteudo"></span></p></p></div>
+              <div class="col-xm-6 col-sm-6 col-md-6 col-lg-6"><p><strong>Enviado em:</strong> <span id="data_envio"></span></p></p></div>
+              <div class="col-xm-6 col-sm-6 col-md-6 col-lg-6"><p><strong>Alterado em:</strong> <span id="data_alteracao"></span></p></p></div>
+              <div class="col-xm-12 col-sm-12 col-md-12 col-lg-12"><p><strong>Conteúdo:</strong> <span id="conteudo"></span></p></p></div>
             </div>
           </div>
         </div>
